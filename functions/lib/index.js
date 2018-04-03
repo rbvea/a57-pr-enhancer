5716a063d74d321da25d06206deca370edae53c5
rbvea <russellbvea@gmail.com>
2018-04-02 22:52:54 -1000

    updated parser WIP

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -1,7 +1,59 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const firebase = require("firebase-functions");
-exports.handlePullRequest = firebase.https.onRequest((req, res) => {
-    res.send("Hello, World!");
-});
+const node_fetch_1 = require("node-fetch");
+const dotenv = require("dotenv");
+dotenv.config();
+exports.handlePullRequest = firebase.https.onRequest((req, res) => __awaiter(this, void 0, void 0, function* () {
+    let { action, pull_request } = JSON.parse(req.body.payload);
+    if (action !== "opened") {
+        res.send("Not creating a pull request");
+        return;
+    }
+    let github_api_key = firebase.config().app.github_api_key;
+    let headers = { Authorization: `Bearer ${github_api_key}` };
+    let issueBody = `
+    query { 
+      viewer { 
+        organization(login: "Atlantic57") {
+          repository(name: "a57-hugo") {
+            issues(labels: ["QA"], first: 50) {
+              nodes {
+                title
+                number
+              }
+            }
+          }
+        }
+      }
+    }`;
+    let issuesRes = yield node_fetch_1.default("https://api.github.com/graphql", {
+        headers,
+        issueBody
+    });
+    let issues = issuesRes.json();
+    let pullRequestBody = issues.data.viewer.organization.repository.issues.nodes
+        .map(({ title, number }) => {
+        return `${title} [#${number}]`;
+    })
+        .join("\n");
+    let updateRes = yield node_fetch_1.default(`https://api.github.com/repos/${pull_request.repository.full_name}/pulls/${pull_request.number}`, {
+        method: "PATCH",
+        headers,
+        body: pullRequestBody
+    });
+    if (updateRes.ok) {
+        res.send("Success!");
+        return;
+    }
+    res.send("An error occurred");
+}));
 //# sourceMappingURL=index.js.map
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,56 @@
 import * as firebase from "firebase-functions";
+import fetch, { Headers } from "node-fetch";
 
-export const handlePullRequest = firebase.https.onRequest((req, res) => {
-  res.send("Hello, World!");
+import * as dotenv from "dotenv";
+dotenv.config();
+
+export const handlePullRequest = firebase.https.onRequest(async (req, res) => {
+  let { action, pull_request } = JSON.parse(req.body.payload);
+  if (action !== "opened") {
+    res.send("Not creating a pull request");
+    return;
+  }
+  let github_api_key = firebase.config().app.github_api_key;
+  let headers = { Authorization: `Bearer ${github_api_key}` };
+  let issueBody = `
+    query { 
+      viewer { 
+        organization(login: "Atlantic57") {
+          repository(name: ${pull_request.repository.name}) {
+            issues(labels: ["QA"], first: 50) {
+              nodes {
+                title
+                number
+              }
+            }
+          }
+        }
+      }
+    }`;
+  let issuesRes = await fetch("https://api.github.com/graphql", {
+    headers,
+    issueBody
+  });
+  let parsedIssues = issuesRes.json();
+  let { issues } = parsedIssues.data.viewer.organization.repository.
+  let pullRequestBody = issues.data.viewer.organization.repository.issues.nodes
+    .map(({ title, number }) => {
+      return `${title} [#${number}]`;
+    })
+    .join("\n");
+  let updateRes = await fetch(
+    `https://api.github.com/repos/${pull_request.repository.full_name}/pulls/${
+      pull_request.number
+    }`,
+    {
+      method: "PATCH",
+      headers,
+      body: { body: pullRequestBody }
+    }
+  );
+  if (updateRes.ok) {
+    res.send("Success!");
+    return;
+  }
+  res.send("An error occurred");
 });
